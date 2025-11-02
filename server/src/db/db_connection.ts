c59c1b5c1a13c01afdfaf1319dc41af9627e640e
Saba125 <jiadzeb@gmail.com>
2025-11-03 00:23:54 +0400

    საბა: მც

--- server/src/db/db_connection.ts
+++ server/src/db/db_connection.ts
@@ -1,13 +1,14 @@
 import mysql2 from "mysql2/promise";
 import config from "../lib/config";
 console.log(config);
-const pool: mysql2.Pool = mysql2.createPool({
-  host: config.DB_HOST,
-  database: config.DB_DATABASE,
-  user: config.DB_USER,
-  password: config.DB_PASSWORD,
-  waitForConnections: true,
-  connectionLimit: 10,
-  connectTimeout: 10000, // 10s timeout
-});
+// const pool: mysql2.Pool = mysql2.createPool({
+//   host: config.DB_HOST,
+//   database: config.DB_DATABASE,
+//   user: config.DB_USER,
+//   password: config.DB_PASSWORD,
+//   waitForConnections: true,
+//   connectionLimit: 10,
+//   connectTimeout: 10000, // 10s timeout
+// });
+const pool: mysql2.Pool = mysql2.createPool(config.DB_HOST);
 export default pool;
