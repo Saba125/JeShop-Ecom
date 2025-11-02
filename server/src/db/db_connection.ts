2ca221016f67218fa0f11394afc6b991dfa5d495
Saba125 <jiadzeb@gmail.com>
2025-11-03 00:20:29 +0400

    საბა: მც

--- server/src/db/db_connection.ts
+++ server/src/db/db_connection.ts
@@ -1,15 +1,13 @@
 import mysql2 from "mysql2/promise";
 import config from "../lib/config";
 console.log(config);
-// const pool: mysql2.Pool = mysql2.createPool({
-//   host: config.DB_HOST,
-//   database: config.DB_DATABASE,
-//   user: config.DB_USER,
-//   password: config.DB_PASSWORD,
-//   waitForConnections: true,
-//   connectionLimit: 10,
-//   connectTimeout: 10000, // 10s timeout
-// });
-console.log(process.env.DATABASE_URL);
-const pool = mysql2.createPool(process.env.DATABASE_URL as string);
+const pool: mysql2.Pool = mysql2.createPool({
+  host: config.DB_HOST,
+  database: config.DB_DATABASE,
+  user: config.DB_USER,
+  password: config.DB_PASSWORD,
+  waitForConnections: true,
+  connectionLimit: 10,
+  connectTimeout: 10000, // 10s timeout
+});
 export default pool;
